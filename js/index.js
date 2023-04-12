8590d87296a3edfea1219145ff9307ec31a43835
zesanahmed <zesanahmed593@gmail.com>
2023-04-12 23:18:31 +0600

    card added

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 const loadAllData = async () => {
-    const url = `https://openapi.programming-hero.com/api/ai/tools`;
-    const res = await fetch(url);
-    const data = await res.json();
-    displayAllData(data.data.tools);
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayAllData(data.data.tools.slice(0, 6));
 }
 
 
 const displayAllData = (features) => {
-    const allData = document.getElementById('all-data-container');
-    // console.log(features);
-    features.forEach(feature => {
-        // console.log(feature)
-        const div = document.createElement('div');
-        div.innerHTML = `
-        <div class="card w-96 bg-base-100 shadow-xl">
+  const allData = document.getElementById('all-data-container');
+  // console.log(features);
+  features.forEach(feature => {
+    // console.log(feature)
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <div class="card w-full bg-base-100 shadow-xl">
   <figure class="px-10 pt-10">
-    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+    <img src="${feature.image}" alt="Shoes" class="rounded-xl" />
   </figure>
   <div class="card-body items-center text-center">
     <h2 class="card-title">Shoes!</h2>
@@ -26,8 +26,8 @@ const displayAllData = (features) => {
   </div>
 </div>
         `
-        allData.appendChild(div);
-    });
+    allData.appendChild(div);
+  });
 }
 
 
