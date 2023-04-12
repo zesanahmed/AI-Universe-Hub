7a45c214c49889ef9e83f7db314d280f78250e0a
zesanahmed <zesanahmed593@gmail.com>
2023-04-12 23:45:56 +0600

    dynamic data added

--- js/index.js
+++ js/index.js
@@ -10,18 +10,27 @@ const displayAllData = (features) => {
   const allData = document.getElementById('all-data-container');
   // console.log(features);
   features.forEach(feature => {
-    // console.log(feature)
+    console.log(feature)
     const div = document.createElement('div');
     div.innerHTML = `
         <div class="card w-full bg-base-100 shadow-xl">
   <figure class="px-10 pt-10">
     <img src="${feature.image}" alt="Shoes" class="rounded-xl" />
   </figure>
-  <div class="card-body items-center text-center">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions">
-      <button class="btn btn-primary">Buy Now</button>
+  <div class="card-body">
+    <h2 class="card-title">Features</h2>
+    <p>1. ${feature.features[0]}</p>
+    <p>2. ${feature.features[1]}</p>
+    <p>3. ${feature.features[2]}</p>
+    <hr>
+    <div class="flex flex-row">
+      <div class="basis-3/4">
+        <p class="card-title"> ${feature.name}</p>
+        <p> ${feature.published_in}</p>
+      </div>
+      <div class="card-actions basis-3/12">
+      <button class="btn btn-primary">Details</button>
+    </div>
     </div>
   </div>
 </div>
