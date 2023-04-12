3fdd25f367963487a7145a32fddf5f6423a237aa
zesanahmed <zesanahmed593@gmail.com>
2023-04-12 23:08:30 +0600

    displayAllData added

--- js/index.js
+++ js/index.js
@@ -2,8 +2,34 @@ const loadAllData = async () => {
     const url = `https://openapi.programming-hero.com/api/ai/tools`;
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data)
+    displayAllData(data.data.tools);
 }
+
+
+const displayAllData = (features) => {
+    const allData = document.getElementById('all-data-container');
+    // console.log(features);
+    features.forEach(feature => {
+        // console.log(feature)
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card w-96 bg-base-100 shadow-xl">
+  <figure class="px-10 pt-10">
+    <img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" class="rounded-xl" />
+  </figure>
+  <div class="card-body items-center text-center">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+        `
+        allData.appendChild(div);
+    });
+}
+
 
 loadAllData();
 
